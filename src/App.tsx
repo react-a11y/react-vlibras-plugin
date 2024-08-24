f4a6e3c75499fdb24f20d1a40b63f17f79d3f30c
lucasferreiralimax <lucasferreiralimax@gmail.com>
2024-08-24 13:19:02 -0300

    initial

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import reactLogo from '/react.svg'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>React</h1>
+    </>
+  )
+}
+
+export default App
