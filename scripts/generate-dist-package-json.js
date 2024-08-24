2f0f71061df74ef9853340548d46a21dad5277aa
lucasferreiralimax <lucasferreiralimax@gmail.com>
2024-08-24 17:21:07 -0300

    adjustment output package public

--- scripts/generate-dist-package-json.js
+++ scripts/generate-dist-package-json.js
@@ -6,10 +6,11 @@ import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+// Resolve the output directory path
 const outputDir = path.resolve(__dirname, '../dist');
 
 // Read the current package.json
-const pkg = JSON.parse(fs.readFileSync('./package.json', 'utf-8'));
+const pkg = JSON.parse(fs.readFileSync(path.resolve(__dirname, '../package.json'), 'utf-8'));
 
 // Modify the package.json for the dist folder
 const distPackageJson = {
@@ -20,10 +21,17 @@ const distPackageJson = {
   peerDependencies: {
     react: '^17.0.0 || ^18.0.0',
   },
-  files: pkg.files,
-  types: pkg.types,
-  main: pkg.main,
-  exports: pkg.exports,
+  files: [
+    'react-vlibras-plugin.js'
+  ],
+  main: './react-vlibras-plugin.js',
+  types: './main.d.ts',
+  exports: {
+    ".": {
+      "import": './react-vlibras-plugin.js',
+      "require": './react-vlibras-plugin.js'
+    }
+  }
 };
 
 // Write the new package.json to the dist folder
@@ -32,10 +40,8 @@ fs.writeFileSync(
   JSON.stringify(distPackageJson, null, 2)
 );
 
-// Copy the README.md file to the dist folder
-const readmePath = path.resolve(__dirname, '../README.md');
-const distReadmePath = path.join(outputDir, 'README.md');
-
-fs.copyFileSync(readmePath, distReadmePath);
-
 console.log('Dist package.json generated successfully.');
+
+// Optionally copy README
+fs.copyFileSync(path.resolve(__dirname, '../README.md'), path.join(outputDir, 'README.md'));
+console.log('README.md copied to dist.');
