bf437beb9f631c7844f32103a201c9425e221f78
lucasferreiralimax <lucasferreiralimax@gmail.com>
2024-08-24 13:56:21 -0300

    adjustment config module

---
+++ vite-module.config.ts
@@ -0,0 +1,28 @@
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/main-module.tsx'),
+      name: 'ReactVLibras',
+      // the proper extensions will be added
+      fileName: 'react-vlibras-plugin'
+    },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: ['react'],
+      output: {
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {
+          react: 'react'
+        }
+      }
+    }
+  }
+})
