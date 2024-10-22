9522b08fca67ee7d951d2a460ebc598526bdaf95
lucasferreiralimax <lucasferreiralimax@gmail.com>
2024-10-22 05:40:29 -0300

    implementation of nucleus-react-js

---
+++ src/App.config.ts
@@ -0,0 +1,23 @@
+import pkg from "../package.json";
+
+export const configApp = {
+  name: "react-vlibras-plugin",
+  npm: "https://www.npmjs.com/package/react-vlibras-plugin",
+  github: "https://github.com/criar-art/react-vlibras",
+  appVersion: pkg.version,
+  reactVersion: pkg.dependencies.react.replace('^', ''),
+  stepsInstall: [
+    {
+      name: 'Install',
+      language: 'bash',
+      content: 'npm install react-vlibras-plugin',
+    },
+    {
+      name: 'Usage',
+      language: 'tsx',
+      content: `import ReactVLibras from 'react-vlibras-plugin'
+
+<ReactVLibras position="left" />`,
+    },
+  ],
+}
