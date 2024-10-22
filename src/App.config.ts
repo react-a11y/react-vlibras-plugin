331ddd4065da2f5855cf154c318a47a9c4179649
lucasferreiralimax <lucasferreiralimax@gmail.com>
2024-10-22 06:09:29 -0300

    add plugin name in github

--- src/App.config.ts
+++ src/App.config.ts
@@ -3,7 +3,7 @@ import pkg from "../package.json";
 export const configApp = {
   name: "react-vlibras-plugin",
   npm: "https://www.npmjs.com/package/react-vlibras-plugin",
-  github: "https://github.com/criar-art/react-vlibras",
+  github: "https://github.com/criar-art/react-vlibras-plugin",
   appVersion: pkg.version,
   reactVersion: pkg.dependencies.react.replace('^', ''),
   stepsInstall: [
