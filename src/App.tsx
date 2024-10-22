941cb01e05eccaabe412fc698049e160d0a53fc4
lucasferreiralimax <lucasferreiralimax@gmail.com>
2024-10-22 06:14:15 -0300

    add example spa

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,48 @@
 import ReactVLibras from 'react-vlibras-plugin';
 import NucleusReact from 'nucleus-react-js';
 import { configApp } from './App.config';
+import "./App.scss";
 
 function App() {
   return (
     <NucleusReact config={configApp}>
       <ReactVLibras />
+      <div className="content example">
+        <h2>Usage Example</h2>
+
+        <h3>Widget Position</h3>
+        <p>You can set the widget's position using the <code>position</code> attribute. For example:</p>
+        <pre><code>&lt;react-vlibras-plugin position="left" /&gt;</code></pre>
+
+        <h3>Available Position Types</h3>
+        <p>The position options that can be used are:</p>
+        <ul>
+          <li>left</li>
+          <li>right</li>
+          <li>top</li>
+          <li>bottom</li>
+          <li>bottom-left</li>
+          <li>top-left</li>
+          <li>bottom-right</li>
+          <li>top-right</li>
+        </ul>
+
+        <h3>Avatar</h3>
+        <p>You can select the widget's avatar using the <code>avatar</code> attribute. Example:</p>
+        <pre><code>&lt;react-vlibras-plugin avatar="hosana" /&gt;</code></pre>
+
+        <h3>Available Avatar Options</h3>
+        <ul>
+          <li>icaro</li>
+          <li>hosana</li>
+          <li>guga</li>
+          <li>random</li>
+        </ul>
+
+        <h3>Opacity</h3>
+        <p>To adjust the widget's opacity, use the <code>opacity</code> attribute:</p>
+        <pre><code>&lt;react-vlibras-plugin opacity="0.8" /&gt;</code></pre>
+      </div>
     </NucleusReact>
   )
 }
